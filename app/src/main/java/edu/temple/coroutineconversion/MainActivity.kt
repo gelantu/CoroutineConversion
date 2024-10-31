e6a793029689b1618fda1f368ca6fb1638c639cf
Gelan Balcha <tuo96713@temple.edu>
2024-10-31 12:01:15 -0400

    Replace Thread code with coroutines

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -7,6 +7,12 @@ import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
@@ -31,14 +37,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
-                repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
+        findViewById<Button>(R.id.revealButton).setOnClickListener {
+            val scope = CoroutineScope(Job() + Dispatchers.Default)
+            scope.launch {
+                repeat(100) { opacity -> withContext(Dispatchers.Main) {
+                        currentTextView.text =
+                            String.format(Locale.getDefault(), "Current opacity: %d", opacity)
+                        cakeImageView.alpha = opacity / 100f
+                    }
+                    delay(40)
                 }
-            }.start()
+            }
         }
     }
+
+
+
 }
